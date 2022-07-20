92b6caa397777349fe3e4eba291c857a9cd46ecb
brs14shn <brsshn292929@gmail.com>
2022-07-20 22:48:32 +0300

    MovieDetails

--- src/context/AutoContextProvider.jsx
+++ src/context/AutoContextProvider.jsx
@@ -1,9 +1,19 @@
 import React, { useState } from "react";
+import { useContext } from "react";
 import { useEffect } from "react";
 import { createContext } from "react";
 import { userObserver } from "../auth/firebase";
 
+//? 1-Defining
 export const AuthContext = createContext();
+
+//? 3-Consume function (Custom Hook)
+
+export const useAuthContext = () => {
+  return useContext(AuthContext);
+};
+
+//? 2- Provider Component
 
 const AutoContextProvider = ({ children }) => {
   console.log(children);
